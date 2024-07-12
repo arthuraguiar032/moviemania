ae2d220263a8bfa62f28623f171e36f677399b01
aguiar.arthur <aguiar.arthur@graduacao.uerj.br>
2024-07-12 17:16:46 -0300

    Comecando a fazer a interface das infos dos filmes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import MovieContent from './components/MovieContent';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieContent />
       <h1>React App</h1>
     </div>
   );
---
+++ src/components/MovieContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/moviecontent.scss'
+
+function MovieContent() {
+  return (
+    <>
+        <div className="imageMovie">
+        </div>
+    </>
+  )};
+
+export default MovieContent;
