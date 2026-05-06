8ad7e4d094b1b111eabddbda74327dd3552620da
Arthur Aguiar <aguiarthur.32@gmail.com>
2026-05-05 23:15:37 -0300

    feat: adiciona footer estilizado

--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -1,13 +1,40 @@
-
-import styles from "./Footer.module.css";
+import Link from 'next/link';
+import styles from './Footer.module.css';
 
 const Footer = () => {
-
-    return (
-        <footer className={styles.footer}>
-            Todos os direitos reservados &copy; 2026   |   Desenvolvido por Arthur Aguiar, Argos Maia e Marcos Mendonça.
-        </footer>
-    );
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <div className={styles.footerSection}>
+          <h3 className={styles.footerTitle}>MovieMania</h3>
+          <p className={styles.footerDescription}>
+            Sua plataforma favorita para descobrir e explorar o mundo do cinema.
+          </p>
+        </div>
+        
+        <div className={styles.footerSection}>
+          <h4 className={styles.footerSubtitle}>Links Rápidos</h4>
+          <div className={styles.footerLinks}>
+            <Link href="/" className={styles.footerLink}>Home</Link>
+            <Link href="/movie/popular" className={styles.footerLink}>Filmes Populares</Link>
+            <Link href="/movie/top_rated" className={styles.footerLink}>Top Filmes</Link>
+          </div>
+        </div>
+        
+        <div className={styles.footerSection}>
+          <h4 className={styles.footerSubtitle}>Informações</h4>
+          <div className={styles.footerLinks}>
+            <Link href="/about" className={styles.footerLink}>Sobre</Link>
+            <Link href="/contact" className={styles.footerLink}>Contato</Link>
+          </div>
+        </div>
+      </div>
+      
+      <div className={styles.footerBottom}>
+        <p>&copy; 2023 MovieMania. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
