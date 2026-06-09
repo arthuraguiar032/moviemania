d7dbed43c6b13ae3bbafcd983e7c38cb1dde5069
Arthur Aguiar <aguiarthur.32@gmail.com>
2026-06-08 23:25:03 -0300

    feat: adiciona componente estilizado de TagButton com link

---
+++ src/components/ui/TagButton/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from './TagButton.module.css';
+
+const TagButton = ({label, path}) => {
+    return (
+        <Link href={path} className={styles.tag}>
+            {label}
+        </Link>
+    )
+};
+
+export default TagButton;
