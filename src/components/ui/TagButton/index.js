d37a707b58a5689dc50091e5cc38264e35b364ce
Arthur Aguiar <aguiarthur.32@gmail.com>
2026-06-09 17:08:05 -0300

    feat: adiciona componente de botao de tag

--- src/components/ui/TagButton/index.js
+++ src/components/ui/TagButton/index.js
@@ -3,10 +3,10 @@ import styles from './TagButton.module.css';
 
 const TagButton = ({label, path}) => {
     return (
-        <Link href={path} className={styles.tag}>
-            {label}
-        </Link>
-    )
+      <Link href={path} className={styles.tag}>
+        {label}
+      </Link>
+    );
 };
 
 export default TagButton;
