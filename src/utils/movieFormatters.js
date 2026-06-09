2e9e43ba0fdcbe2f134e4d96bdd7400335ef5c9a
Arthur Aguiar <aguiarthur.32@gmail.com>
2026-06-08 23:27:27 -0300

    feat: adiciona função em utils para formatar moeda de acordo com localização do client

--- src/utils/movieFormatters.js
+++ src/utils/movieFormatters.js
@@ -4,3 +4,19 @@ export const truncate_decimal = (num, places) => {
     const multiplier = Math.pow(10, places);
     return Math.trunc(num * multiplier) / multiplier;
   };
+
+export const format_currency = (value, locale) => {
+  
+  const currencies = {
+    'pt-BR': 'BRL', // Brasil
+    'en-US': 'USD', // Estados Unidos
+    'es-ES': 'EUR'  // Espanha/União Europeia
+  };
+
+  const currency = currencies[locale] || 'USD';
+
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(value);
+};
