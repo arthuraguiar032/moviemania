5870009f8d908fa14f95471521ac172fbae725a0
Arthur Aguiar <aguiarthur.32@gmail.com>
2026-06-09 20:17:50 -0300

    WIP: camada de service para info de moviepage

--- src/service/tmdb_movieInfo.js
+++ src/service/tmdb_movieInfo.js
@@ -1,6 +1,6 @@
 import axiosInstance, { DEFAULT_PARAMS } from "./axios";
 
-const movieInfoRequest = async (endpoint, customParams) => {
+const movieInfoRequest = async (endpoint, customParams, movie_id) => {
     const config = {
         params: {
             ...DEFAULT_PARAMS,
@@ -8,7 +8,22 @@ const movieInfoRequest = async (endpoint, customParams) => {
         },
     };
 
-    const response = await axiosInstance.get();//
+    const response = await axiosInstance.get(`/movie/${movie_id}${endpoint}`, config);
     return response.data;
 };
 
+export const movieInfoService = {
+  getDetails: (movie_id, params = {}) => movieInfoRequest("", params, movie_id),
+
+  getCredits: (movie_id, params = {}) =>
+    movieInfoRequest("/credits", params, movie_id),
+
+  getKeywords: (movie_id, params = {}) =>
+    movieInfoRequest("/keywords", params, movie_id),
+
+  getRecommendations: (movie_id, params = {}) =>
+    movieInfoRequest("/recommendations", params, movie_id),
+
+  getSimilar: (movie_id, params = {}) =>
+    movieInfoRequest("/similar", params, movie_id),
+};
